a1940b713ebb36b433cabb94576614496ccd7f45
Алексей Рогатин <bamako99@mail.ru>
2020-08-29 18:28:17 +0300

    Добавлен фильтр поиска. Теперь в классе находятся два массива, храниящие объекты Recipe. Один из них служит для отображения объектов пользователю, другой используется при поиске для отображения первоначального состояния списка без фильтра

--- app/src/main/java/com/example/recipes/RecipesAdapter.kt
+++ app/src/main/java/com/example/recipes/RecipesAdapter.kt
@@ -1,34 +1,45 @@
 package com.example.recipes
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipes.utils.convertToBitmap
+import com.example.recipes.utils.second
 import com.google.android.material.snackbar.Snackbar
 import de.hdodenhof.circleimageview.CircleImageView
-import kotlinx.android.synthetic.main.activity_main.*
 
-class RecipesAdapter(private var recipes: ArrayList<Recipe>, private val context: Context) : RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>() {
+class RecipesAdapter(private var recipesForUI: ArrayList<Recipe>, private val context: Context) : RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>(), Filterable {
 
     private val dataBaseHelper = DataBaseHelper(context)
+    private var recipesAll : ArrayList<Recipe> = dataBaseHelper.getRecipes()
 
     inner class RecipesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val recipeName: TextView = itemView.findViewById(R.id.recipe_name)
         private val recipeDescription: TextView = itemView.findViewById(R.id.recipe_description)
         private val recipeImage: CircleImageView = itemView.findViewById(R.id.civ_recipe_image)
+        private val recipeIngredients: TextView = itemView.findViewById(R.id.tv_ingredients_string)
 
+        @SuppressLint("SetTextI18n")
         fun bind(position: Int) {
-            val recipe = recipes[position]
+            updateRecipesAll()
+            val recipe = recipesForUI[position]
 
             recipeName.text = recipe.name
             recipeDescription.text = recipe.description
             recipeImage.setImageBitmap(convertToBitmap(recipe.picture))
+
+            if (recipe.ingredients.size > 1) {
+                recipeIngredients.text = "${recipe.ingredients.toString().second().toUpperCase()}${recipe.ingredients.toString().subSequence(2, recipe.ingredients.toString().length - 2)}"
+            } else {
+                recipeIngredients.text = "Ингредиенты не указаны"
+            }
         }
     }
 
@@ -36,22 +47,59 @@ class RecipesAdapter(private var recipes: ArrayList<Recipe>, private val context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = RecipesViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recipy_item, parent, false))
 
-    override fun getItemCount() = recipes.size
+    override fun getItemCount() = recipesForUI.size
+
+    override fun getFilter() = filter
+
+    private val filter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+
+            val filteredRecipesList = arrayListOf<Recipe>()
+
+            if (constraint.toString().isEmpty()) {
+                filteredRecipesList.addAll(recipesAll)
+            } else {
+                for (ingredient in recipesAll) {
+                    if (ingredient.ingredients.toString().toLowerCase().contains(constraint.toString().toLowerCase())) {
+                        filteredRecipesList.add(ingredient)
+                    }
+                }
+            }
+
+            val filterResults = FilterResults()
+            filterResults.values = filteredRecipesList
+
+            return filterResults
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            recipesForUI.clear()
+            recipesForUI.addAll(results?.values as Collection<Recipe>)
+            notifyDataSetChanged()
+        }
+
+    }
 
     fun removeRecipe(viewHolder: RecyclerView.ViewHolder) {
         val removedPosition = viewHolder.adapterPosition
-        val removedItem = recipes[removedPosition]
+        val removedItem = recipesForUI[removedPosition]
 
-        dataBaseHelper.removeRecipe(recipes[removedPosition].name)
+        dataBaseHelper.removeRecipe(recipesForUI[removedPosition].name)
 
-        recipes.removeAt(removedPosition)
+        recipesForUI.removeAt(removedPosition)
+        updateRecipesAll()
         notifyItemRemoved(removedPosition)
 
         Snackbar.make(viewHolder.itemView, "Удалено", Snackbar.LENGTH_LONG)
             .setAction("Отменить") {
                 dataBaseHelper.restoreRecipe()
-                recipes.add(removedPosition, removedItem)
+                recipesForUI.add(removedPosition, removedItem)
+                updateRecipesAll()
                 notifyItemInserted(removedPosition)
             }.show()
+    }
+
+    private fun updateRecipesAll() {
+        recipesAll = dataBaseHelper.getRecipes()
     }
 }
