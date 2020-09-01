e2f8e99195a778c75ecd3336b4d856e205c64dd7
Alex <bamako99@mail.ru>
2020-09-01 16:04:54 +0300

    Класс теперь использует интерфейс Serializable для передачи объектов класса через intent

--- app/src/main/java/com/example/recipes/Recipe.kt
+++ app/src/main/java/com/example/recipes/Recipe.kt
@@ -1,3 +1,5 @@
 package com.example.recipes
 
-class Recipe(val name: String, val description: String, val ingredients: ArrayList<String>, val picture: String)
+import java.io.Serializable
+
+class Recipe(val name: String, val description: String, val ingredients: ArrayList<String>, val picture: String) : Serializable
