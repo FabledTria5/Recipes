c8325f3960742e28e0290e150fab966a2994d4fa
Алексей Рогатин <bamako99@mail.ru>
2020-10-08 13:03:54 +0300

    Добалено новое поле для подсчета ингредиентов

--- app/src/main/java/com/example/recipes/Recipe.kt
+++ app/src/main/java/com/example/recipes/Recipe.kt
@@ -2,4 +2,4 @@ package com.example.recipes
 
 import java.io.Serializable
 
-class Recipe(val name: String, val description: String, val ingredients: ArrayList<String>, val picture: String, val fullRecipe: String) : Serializable
+class Recipe(val name: String, val description: String, val ingredients: ArrayList<String>, val ingredientsAmount: ArrayList<String>, val picture: String, val fullRecipe: String) : Serializable
